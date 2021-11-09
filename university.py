f121dd9b1ac5898b36d15f0a4257bfe628f96b07
fvn70 <fvn_reg@mail.ru>
2021-11-09 22:19:14 +0700

    Stage 6/7: Extensive training

--- university.py
+++ university.py
@@ -1,6 +1,7 @@
 from copy import copy
 
-departments = {'Biotech', 'Chemistry', 'Engineering', 'Mathematics', 'Physics'}
+departments = {'Biotech': [0, 1], 'Chemistry': [1], 'Engineering': [2, 3], 'Mathematics': [2], 'Physics': [0, 2]}
+exams = {'physics', 'chemistry', 'math', 'computer science'}
 
 def load_data():
     list = []
@@ -9,35 +10,54 @@ def load_data():
             list.append(ln.split())
     return list
 
-def sort_gpa():
-    apps.sort(key=lambda x: (-float(x[2]), x[0], x[1]))
+# k in [0..3] - exams list
+def sort_exam(list, ex):
+    list.sort(key=lambda x: (-mean_score(x, ex), x[0], x[1]))
 
-# d - department, k = 1..3 - rank
-def choise_by_rank(d, k):
+# a - student info, ex - exams list
+def mean_score(a, ex):
+    sum = 0
+    for x in ex:
+        sum += float(a[x + 2])
+    return sum / len(ex)
+
+# d - department, 1..3 - rank
+def choise_by_rank(d, rank):
+    ex = departments[d]
     n1 = n - len(map_by_dep[d])
-    app_d = [a for m, a in enumerate(filter(lambda x: x[k + 2] == d, apps)) if m < n1]
+    sort_exam(apps, ex)
+    app_d = [a for m, a in enumerate(filter(lambda x: x[rank + 5] == d, apps)) if m < n1]
     map_by_dep[d] += app_d
     for a in app_d:
         apps.remove(a)
     if len(map_by_dep[d]) == n:
         dep_to_do.remove(d)
-    # print(f"{d}: new={len(app_d)}, N={len(map_by_dep[d])}")
+
+def save_data():
+    for d in sorted(map_by_dep):
+        ex = departments[d]
+        sort_exam(map_by_dep[d], ex)
+        fn = d.lower() + ".txt"
+        with open(fn, "w") as file:
+            for a in map_by_dep[d]:
+                file.write(f"{a[0]} {a[1]} {mean_score(a, ex)}\n")
 
 
 n = int(input())
 apps = load_data()
-sort_gpa()
 map_by_dep = {d: [] for d in departments}
-dep_to_do = copy(departments)
+dep_to_do = list(departments.keys())
 rank = 0
 while apps and dep_to_do and rank < 3:
     rank += 1
     dep = copy(dep_to_do)
     for d in dep:
         choise_by_rank(d, rank)
+save_data()
+# for d in sorted(map_by_dep):
+#     print(f"\n{d}")
+#     ex = departments[d]
+#     sort_exam(map_by_dep[d], ex)
+#     for a in map_by_dep[d]:
+#         print(f"{a[0]} {a[1]} {mean_score(a, ex)}")
 
-for d in sorted(map_by_dep):
-    print(f"\n{d}")
-    map_by_dep[d].sort(key=lambda x: (-float(x[2]), x[0], x[1]))
-    for a in map_by_dep[d]:
-        print(*a[:3])
