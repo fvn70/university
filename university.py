c4282bb90866542ad761b50d3464797e2013685f
fvn70 <fvn_reg@mail.ru>
2022-03-07 08:08:28 +0700

    Stage 2/7: Raising the bar

--- university.py
+++ university.py
@@ -2,4 +2,8 @@ x1 = int(input())
 x2 = int(input())
 x3 = int(input())
 y = (x1 + x2 + x3) / 3
-print(f'{y}\nCongratulations, you are accepted!')
+print(y)
+if y >= 60:
+    print('Congratulations, you are accepted!')
+else:
+    print('We regret to inform you that we will not be able to offer you admission.')
